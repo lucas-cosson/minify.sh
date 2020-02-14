9daed4bbc1b9b8b6a70ef6df658b1e47bbe0efbd
Nathanaël <contact@nathanaelhoun.fr>
2020-02-14 16:33:05 +0100

    minify_html function

--- html_function.sh
+++ html_function.sh
@@ -1,14 +1,37 @@
 #!/bin/dash
 
-minify_html () {    
+# $1 : path to an .html file
+# $2 : optional, path to tags_file
+# output the minified text to the standart output
+minify_html () {
     if [ "${1%.html}" = "$1" ]; then
-        echo "DEV : this is not a .html file"
+      echo "DEV : this is not a .html file" >&2
+      exit 1
+    fi
+
+    local ENABLE_TAG
+    ENABLE_TAG=false
+    if [ -n "$2" ]; then
+      if [ ! -f "$2" ]; then
+        echo "DEV : tag_file is not a file" >&2
         exit 1
+      fi
+      ENABLE_TAG=true
     fi
 
     local FILE_CONTENT
     FILE_CONTENT=$(tr '\n' ' ' < "$1");
-    echo $FILE_CONTENT
+    FILE_CONTENT=$(echo -n "$FILE_CONTENT" | sed -E 's/<!--[^-]*-->//g') # TODO : marche pour tous sauf les commentaires qui contiennent des tirets
+    FILE_CONTENT=$(echo -n "$FILE_CONTENT" | sed -E 's/[[:space:]]+/ /g' ) # inutile sauf pour \v, echo fait le découpage avec l'IFS
+
+    if $ENABLE_TAG; then
+      for TAG in $(cat "$2"); do
+        FILE_CONTENT=$(echo -n "$FILE_CONTENT" | sed -E "s/[[:space:]]*<$TAG>*>[[:space:]]/<$TAG>/g")
+        FILE_CONTENT=$(echo -n "$FILE_CONTENT" | sed -E "s/[[:space:]]*<\/$TAG>[[:space:]]/<\/$TAG>/g")
+      done
+    fi
+    
+    echo "$FILE_CONTENT"
 }
 
-minify_html $1
+minify_html $1 $2
