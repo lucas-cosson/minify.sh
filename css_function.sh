801a47493043945614b02416f17e8f4f4e33e498
EvoXNeo <evox.neo@gmail.com>
2020-02-16 17:15:03 +0100

    Ajout suppression des commentaires CSS

--- css_function.sh
+++ css_function.sh
@@ -7,8 +7,8 @@ minify_css(){
     fi
 
     local FILE_CONTENT
-    FILE_CONTENT=$(grep -E [A-Za-z]\>) < $1
-    FILE_CONTENT=$(tr '\n' ' ' < "$1" | sed -E "s/([[:alnum:]])[ ]*([{;:,>+])[ ]*/\1\2/g" source_test/styles/gazette.css);
+    FILE_CONTENT=$(cat $1 | sed "s#/\*.*\*/\(.\+\)#\1#" | sed -E "/\/\*/,/\*\// d")             #Permet de suprimer les commentaire 1)sur une ligne 2)multiligne
+    FILE_CONTENT=$(echo -n $FILE_CONTENT | sed -E "s/([[:alnum:]])[ ]*([{;:,>+])[ ]*/\1\2/g")   #Permet de supprimer les espaces entre les différents label
     echo $FILE_CONTENT
 }
 
